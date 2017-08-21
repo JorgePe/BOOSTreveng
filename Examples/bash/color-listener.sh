306df363a1c57734cfe1333eb08b80f410404432
Jorge Pereira <JorgePe@users.noreply.github.com>
2017-08-21 16:08:18 +0100

    Update color-listener.sh

--- Examples/bash/color-listener.sh
+++ Examples/bash/color-listener.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #
-# This script conencts to BOOST Move Hub and 
-# configure it to continuous color reading mode
-# Color values are stored in file 'BOOSTcolor.txt'
-# for other script (i.e. color-echo.sh) to use
+# This script conencts to BOOST Move Hub and configures it
+# to continuous color reading mode.
+# Last color index value is stored in file 'BOOSTcolor.txt' so
+# other script (color-echo.sh) can use it
+# Demo video: https://youtu.be/j6QjDPRiW7E
 #
 # activate notifications
 gatttool -i hci0 -b 00:16:53:A4:CD:7E --char-write-req --handle=0x0f --value=0100
